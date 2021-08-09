98800b92c77509b64936635cc3fca31e3916d6b6
PotapSP <zero02006@mail.ru>
2021-08-10 01:39:12 +0300

    Добавьте файлы проекта.

---
+++ CsharpDok1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SmallBasic.Library;
+
+namespace CsharpDok1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Move(100);
+        }
+    }
+}
